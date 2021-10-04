d6dcf69b893dac9fa1da14eed29ae92a55986b59
NazarkoNaN <nazar.ivanyuk@gmail.com>
2021-10-04 19:30:30 +0300

    Додав вже існуючі лаби

---
+++ АД/Lab1.1/Lab1.1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Lab1._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Обрахувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(836, 197);
+            this.textBox.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 236);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(730, 56);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(450, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Відповідь";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(860, 304);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Caculator-1000";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ АД/Lab1.1/Lab1.1/Form1.cs
@@ -0,0 +1,117 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Drawing.Chart;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1._1
+{
+    public partial class Form1 : Form
+    {
+        private List<char> simbolList = new List<char>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < textBox.TextLength; i++)
+                simbolList.Add(textBox.Text[i]);
+
+            simbolList.Sort();
+
+            char previosChar = new char();
+            List<Simbol> Gr = new List<Simbol>();
+            Simbol simbol = new Simbol();
+            int k = 0;
+            foreach (char p in simbolList)
+            {
+                if (p >='\u0021')
+                {
+                    if (k == 0)
+                    {
+                        simbol = new Simbol(p);
+                        Gr.Add(simbol);
+                    }
+                    else if (p == previosChar)
+                    {
+                        Gr[Gr.IndexOf(simbol)].kol++;
+                    }
+                    else if (p != previosChar)
+                    {
+                        simbol = new Simbol(p);
+                        Gr.Add(simbol);
+                    }
+                }
+
+                k++;
+                previosChar = p;
+            }
+
+            exelCreatro(Gr);
+
+            textBox2.Clear();
+            foreach (Simbol p in Gr)
+                textBox2.Text += p.simbl +"-"+p.kol+ "   ";
+
+            simbolList.Clear();
+            Gr.Clear();
+        }
+
+        private async Task exelCreatro(List<Simbol> Gr)
+        {
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            var file = new FileInfo(@".\Lab.xlsx");
+
+            var Data = Gr;
+
+            await SaveExelFile(Data,file);
+        }
+
+        private static async Task SaveExelFile(List<Simbol> data, FileInfo file)
+        {
+            DeliteIfExists(file);
+
+            using(var package = new ExcelPackage(file))
+            {
+                var ws = package.Workbook.Worksheets.Add("MinReport");
+
+                var range = ws.Cells["A1"].LoadFromCollection(data,true);
+                range.AutoFitColumns();
+
+                ExcelBarChart Chart = ws.Drawings.AddChart("barChart", eChartType.BarClustered) as ExcelBarChart;
+                Chart.Title.Text = "Графік";
+                Chart.SetSize(600, 300);
+                Chart.SetPosition(5, 0, 1, 0);
+                var ser1 = (ExcelChartSerie)(Chart.Series.Add(ws.Cells["B1:B"+ (data.Count()+1)],ws.Cells["A1:A"+(data.Count+1)]));
+
+                await package.SaveAsync();
+            }
+        }
+
+        private static void DeliteIfExists(FileInfo file)
+        {
+            if (file.Exists)
+                file.Delete();
+        }
+    }
+}
+
+public class Simbol
+{
+    public char simbl { get; set; }
+    public int kol { get; set; }
+
+    public Simbol(char s) { this.simbl = s; this.kol = 1; }
+
+    public Simbol() { }
+}
